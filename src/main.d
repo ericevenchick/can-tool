4eda61a7e0f66a24d99b615d12894baa15875cb9
Eric Evenchick <eric@evenchick.com>
2013-01-23 18:06:33 -0500

    first commit, builds and blinks

---
+++ src/main.d
@@ -0,0 +1,8 @@
+main.o: main.c /home/eric/code/stellarisware/inc/hw_gpio.h \
+ /home/eric/code/stellarisware/inc/hw_memmap.h \
+ /home/eric/code/stellarisware/inc/hw_sysctl.h \
+ /home/eric/code/stellarisware/inc/hw_types.h \
+ /home/eric/code/stellarisware/driverlib/gpio.h \
+ /home/eric/code/stellarisware/driverlib/pin_map.h \
+ /home/eric/code/stellarisware/driverlib/rom.h \
+ /home/eric/code/stellarisware/driverlib/sysctl.h
---
+++ src/startup_gcc.d
@@ -0,0 +1,2 @@
+startup_gcc.o: startup_gcc.c /home/eric/code/stellarisware/inc/hw_nvic.h \
+ /home/eric/code/stellarisware/inc/hw_types.h
